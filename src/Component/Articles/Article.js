9b5f34d5c63fc52bcbeb3c5e577848f4f80687eb
kaneki <mizraim.martinez@gmail.com>
2021-08-14 17:49:12 -0500

    fix size of card

--- src/Component/Articles/Article.js
+++ src/Component/Articles/Article.js
@@ -4,7 +4,7 @@ import { Card, Col } from "react-bootstrap";
 const Article = ({ item }) => {
 	return (
 		<Col md={6} lg={4} sm={12}>
-			<Card style={{ width: "18rem" }} className="my-4 center-article">
+			<Card className="my-4 center-article">
 				<Card.Img variant="top" src={item.Urlimage} />
 
 				<Card.Body>
