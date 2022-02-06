230c18b45a404204fc5d6aa9e28212219f821c11
kaneki <mizraim.martinez@gmail.com>
2022-02-06 14:20:16 -0600

    add discount

--- src/Component/Articles/Article.js
+++ src/Component/Articles/Article.js
@@ -11,6 +11,9 @@ const Article = ({ item }) => {
 					<Card.Title>{item.Nombre}</Card.Title>
 					<Card.Text>{item.Descripcion}</Card.Text>
 					<p className="precio">{`$${item.Precio}`} </p>
+					<p className="descuento">
+						{`$${item.Precio - (item.Precio * 40) / 100}`}{" "}
+					</p>
 					{/* <Button variant="primary">Go somewhere</Button> */}
 				</Card.Body>
 			</Card>
